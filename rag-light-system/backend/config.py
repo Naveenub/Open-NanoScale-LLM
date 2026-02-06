4d8a5f676e431ddbdf842ae945d4a51e345cd10c
Naveen Badiger <83811961+Naveenub@users.noreply.github.com>
2026-02-06 16:27:06 +0530

    Update config.py

--- rag-light-system/backend/config.py
+++ rag-light-system/backend/config.py
@@ -1,4 +1,7 @@
 import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-VECTOR_DB_PATH = "./chroma_db"
+
+VECTOR_DB_PATH = "./data/chroma"
+EMBEDDING_MODEL = "text-embedding-3-large"
+LLM_MODEL = "gpt-4o-mini"
